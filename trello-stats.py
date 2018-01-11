61fba5aefae890699f4bf3527672dc9ba78a1b74
Andrew Block <andy.block@gmail.com>
2018-01-11 14:20:59 -0600

    Externalized Trello auth and add start date variable

--- trello-stats.py
+++ trello-stats.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python
 
-import json, requests, sys
+import os, json, requests, sys, argparse
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 TRELLO_ORG_NAME = 'redhatcop'
-TRELLO_API_KEY = ''
-TRELLO_API_TOKEN = ''
+TRELLO_API_KEY_NAME = 'TRELLO_API_KEY'
+TRELLO_API_TOKEN_NAME = 'TRELLO_API_TOKEN'
+DEFAULT_START_DATE_MONTH = '03'
+DEFAULT_START_DATE_DAY = '01'
 
 # Search for cards that are done and have been modified in the past 30 days
-TRELLO_SEARCH_QUERY = 'list:Done edited:30'
+TRELLO_SEARCH_QUERY = 'list:Done edited:{0}'
 
+def valid_date(s):
+    try:
+        return datetime.strptime(s, "%Y-%m-%d")
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+def generate_start_date():
+    today_date = datetime.now()
+    target_start_date = datetime.strptime("{0}-{1}-{02}".format(today_date.year, DEFAULT_START_DATE_MONTH, DEFAULT_START_DATE_DAY), "%Y-%m-%d")
+
+    if target_start_date.month < DEFAULT_START_DATE_MONTH:
+        target_start_date = target_start_date - relativedelta(years=1)
+
+    return target_start_date
 
 def get_org_id(session):
     
@@ -17,8 +36,9 @@ def get_org_id(session):
 
     return org_request.json()
 
-def search_cards(session, org_id):
-    card_request = session.get("https://api.trello.com/1/search", params={'query': TRELLO_SEARCH_QUERY, 'idOrganizations': org_id, 'card_fields': 'name,idMembers', 'board_fields': 'name,idOrganization', 'card_board': 'true', 'cards_limit': 1000})
+def search_cards(session, org_id, query):
+    print query
+    card_request = session.get("https://api.trello.com/1/search", params={'query': query, 'idOrganizations': org_id, 'card_fields': 'name,idMembers', 'board_fields': 'name,idOrganization', 'card_board': 'true', 'cards_limit': 1000})
     card_request.raise_for_status()
 
     return card_request.json()
@@ -35,22 +55,36 @@ def encode_text(text):
 
     return text
 
-if not TRELLO_API_KEY or not TRELLO_API_TOKEN:
-    print "Error: Trello API Key and Token are Required!"
+
+trello_api_key = os.environ.get(TRELLO_API_KEY_NAME)
+trello_api_token = os.environ.get(TRELLO_API_TOKEN_NAME)
+
+
+if not trello_api_key or not trello_api_token:
+    print "Error: Trello API Key and API Token are Required!"
     sys.exit(1)
 
+parser = argparse.ArgumentParser(description='Gather Trello Statistics.')
+parser.add_argument("-s","--start-date", help="The start date to query from", type=valid_date)
+args = parser.parse_args()
 
+start_date = args.start_date
+
+if start_date is None:
+    start_date = generate_start_date()
+
+days = (datetime.now() - start_date).days
 
 session = requests.Session()
 session.params = {
-    'key': TRELLO_API_KEY,
-    'token': TRELLO_API_TOKEN,
+    'key': trello_api_key,
+    'token': trello_api_token,
 }
 
 org_response = get_org_id(session)
 org_id = org_response['id']
 
-resp_cards = search_cards(session, org_id)
+resp_cards = search_cards(session, org_id, TRELLO_SEARCH_QUERY.format(days))
 
 cards = {}
 members_cards = {}
