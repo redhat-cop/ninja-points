72cf6eb89c9448f02f873cab85de10bdf220aab9
Andrew Block <andy.block@gmail.com>
2018-03-24 17:42:29 -0500

    Added support to produce output as csv

--- rocketchat.py
+++ rocketchat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, json, requests, sys, argparse, collections, re, operator
+import os, json, requests, sys, argparse, collections, re, operator, csv
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
@@ -76,14 +76,6 @@ def filter_channels(channels, channel_filter):
         else:
             channels.remove(channel)
 
-def get_channel_members_stats(session, channel):
-
-        params = {'roomId': channel['_id']}
-
-        channel_info = session.get("https://{0}/api/v1/channels.members?".format(server), params=params)
-
-        return {'id': channel['_id'], 'name': channel['name'], 'total': channel_info.json()['total']}
-
 def process_item(final_dict, history_type, key):
     if key in final_dict[history_type]:
         final_dict[history_type][key] += 1
@@ -92,9 +84,14 @@ def process_item(final_dict, history_type, key):
 
     final_dict['statistics'][history_type] += 1
 
-def get_channel_history_stats(session, channel, history_days):
-    newest_date = datetime.now().utcnow()
-    oldest_date = newest_date - relativedelta(days=history_days)
+def plural_items(text, obj):
+    if obj is not None and (isinstance(obj, collections.Iterable) and len(obj) == 1) or obj == 1:
+        return text[:-1]
+    else:
+        return text
+
+
+def get_channel_history_stats(session, channel, newest_date, oldest_date):
     formatted_oldest_date = oldest_date.strftime(ROCKETCHAT_TIME_FORMAT)
     formatted_newest_date = newest_date.strftime(ROCKETCHAT_TIME_FORMAT)
     return get_channel_history(session, channel, formatted_oldest_date, formatted_newest_date)
@@ -128,6 +125,19 @@ def get_channel_history(session, channel, oldest_date, newest_date, current_late
     else:
         return final_dict
 
+def write_ouput_file_record(filename, output_file_records, first_record=None):
+
+    mode = 'a' if not first_record else 'w'
+
+    with open(filename, mode) as f:
+        fieldnames = ['Chat Channel (ID)', 'Time Period', '# Users Joined', '# Messages', 'Individual User Data - % messages/channel/user']
+        writer = csv.writer(f)
+
+        if first_record is not None and first_record == True:
+             writer.writerow(fieldnames)
+
+        writer.writerow(output_file_records)
+
 
 rocketchat_username = os.environ.get(ROCKETCHAT_USERNAME)
 rocketchat_password = os.environ.get(ROCKETCHAT_PASSWORD)
@@ -138,11 +148,13 @@ parser = argparse.ArgumentParser(description='Gather Rocketchat Statistics.')
 parser.add_argument("-f","--filter", help="Text in Channel Description to Filter On", required=True)
 parser.add_argument("-d","--days", help="Number of Days to Search for Records", type=int)
 parser.add_argument("-s","--server", help="Rocketchat Server")
+parser.add_argument("-o","--output", help="Output File")
 args = parser.parse_args()
 
 filtered_text = args.filter
 server = args.server
 days = args.days
+output_file = args.output
 
 if not server:
     server = ROCKETCHAT_SERVER_DEFAULT
@@ -162,17 +174,54 @@ channels = get_channels(session, server,)
 
 filter_channels(channels, filtered_text)
 
+newest_date = datetime.now().utcnow()
+oldest_date = newest_date - relativedelta(days=days)
 
-print "=== Rocketchat Statistics For the Past {0} Days ===\n".format(days)
+formatted_time_period = "{0} - {1}".format(oldest_date.strftime("%m/%d/%Y"), newest_date.strftime("%m/%d/%Y"))
+
+print "=== Rocketchat Statistics For {0} ===\n".format(formatted_time_period)
 if len(channels) > 0:
-    for channel in channels:
-        channel_history_stats = get_channel_history_stats(session, channel, days)
-        print "{0}".format(channel['name'])
-        print "  {0} Users Joined".format(channel_history_stats['statistics']['joined'])
-        print "  {0} Users Removed".format(channel_history_stats['statistics']['removed'])
-        print "  {0} Messages".format(channel_history_stats['statistics']['messages'])
+    for channel_index, channel in enumerate(channels):
+        
+        output_file_row_records = []
+
+        channel_history_stats = get_channel_history_stats(session, channel, newest_date, oldest_date)
+
+        formatted_channel_name = "#{0}".format(channel['name'])
+        users_joined = channel_history_stats['statistics']['joined']
+        users_removed = channel_history_stats['statistics']['removed']
+        total_messages = channel_history_stats['statistics']['messages']
+
+        print formatted_channel_name
+        print "  {0} {1} Joined".format(users_joined, plural_items("Users", users_joined))
+        print "  {0} {1} Removed".format(users_removed, plural_items("Users", users_removed))
+        print "  {0} {1}".format(total_messages, plural_items("Messages", total_messages))
+
+        output_file_user_messages = ""
+
         for username, username_num_messages in sorted(channel_history_stats['messages'].iteritems(), key=lambda (k,v): (v,k), reverse=True):
-            print "    * {0} - {1:.2f}% - {2} Messages".format(username, (float(username_num_messages)/float(channel_history_stats['statistics']['messages'])*100), username_num_messages)
+            
+            user_messages = "{0} - {1:.2f}% - {2} {3}".format(username, (float(username_num_messages)/float(total_messages)*100), username_num_messages, plural_items("Messages", username_num_messages))
+            
+            print "    * {0}".format(user_messages)
+
+            if output_file_user_messages is not "":
+                output_file_user_messages += "\n"
+            
+            output_file_user_messages += user_messages
+    
+        if output_file is not None:
+            
+            output_file_row_records.append(formatted_channel_name)
+            output_file_row_records.append(formatted_time_period)
+            output_file_row_records.append(users_joined)
+            output_file_row_records.append(total_messages)
+            output_file_row_records.append(output_file_user_messages)
+
+            if channel_index == 0:
+                write_ouput_file_record(output_file, output_file_row_records, True)
+            else:
+                write_ouput_file_record(output_file, output_file_row_records)
 
 else:
     print "No Rocketchat Channels Match the description '{0}'".format(filtered_text)
